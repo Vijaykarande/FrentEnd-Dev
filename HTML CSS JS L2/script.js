06b589a4f2e33e94ef3eb58ae2ed9d4f1c61a833
Vijaykarande <vijaykarande28@gmail.com>
2024-08-05 22:02:22 +0530

    JS VARIABLE & Objects

---
+++ HTML CSS JS L2/script.js
@@ -0,0 +1,38 @@
+var a=5;
+
+var b=70;
+console.log(a+b);
+var c="Vijay";
+var d= 'd';
+console.log(typeof a, typeof b, typeof c, typeof d)
+
+const a1=60;
+console.log(a1);
+
+{
+    let a=160;
+    a=160+160
+    console.log(a)
+}
+console.log(a)
+
+
+let s="VijayKarande";
+
+let y=23.34;
+let z=12;
+let j=true;
+let r=null;
+let m=undefined;
+
+console.log(typeof s,typeof z,typeof j,typeof r,typeof m);
+
+
+
+let o={name:"VIJAY KARANDE" , Designation:"S D", isHandsome:true};
+console.log(o);
+o.salary="5000Cr"
+console.log(o);
+
+o.bike="RX 100";
+console.log(o);
