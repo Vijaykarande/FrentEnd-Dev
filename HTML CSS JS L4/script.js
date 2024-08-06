f73afa8011836fbd1de4d05d090b96e3b097d9c3
Vijaykarande <vijaykarande28@gmail.com>
2024-08-06 14:43:24 +0530

    JS Loops

---
+++ HTML CSS JS L4/script.js
@@ -0,0 +1,39 @@
+console.log("JS LOOPS")
+
+// let a=2;
+
+// for(let i = 0; i< 100 ; i++)
+// {
+    
+//     console.log(a+i);
+// }
+
+
+// let object ={
+//     name: "Vijay Karande",
+//     Designation: "Developer",
+//     Company:"TCS"
+// }
+
+// for (const key in object) {
+   
+//         const element = object[key];
+
+//         console.log(key , element)
+        
+//     }
+
+
+//     for (const element of "Vijay Karande") {
+//         console.log(element)
+        
+//     }
+
+
+let a=10;
+
+while (a<=10) {
+    console.log(a);
+    a++;
+    
+}
---
+++ HTML CSS JS L4/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+
+let a=10
+
+while (a<=6) {
+    console.log(a)
+    a++;
+    
+}
