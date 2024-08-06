24026a83791b9307431f6a0cd793efff0a82110c
Vijaykarande <vijaykarande28@gmail.com>
2024-08-06 13:59:58 +0530

    JS Conditional Statement

---
+++ HTML CSS JS L3/script.js
@@ -0,0 +1,23 @@
+console.log("Decsion Making Statements")
+
+let a=16;
+let b=8;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+console.log(a++);
+console.log(b--);
+
+if(a>=40)
+{
+    console.log("Greater");
+}
+else
+{
+    console.log("Not Greter");
+}
