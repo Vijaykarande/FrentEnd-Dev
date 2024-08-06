ee9f6168e3adcf643ddf28a343108e0e5677d28c
Vijaykarande <vijaykarande28@gmail.com>
2024-08-06 15:13:37 +0530

    JS Function

---
+++ HTML CSS JS L5/script.js
@@ -0,0 +1,40 @@
+console.log("JS Functions/Methods")
+
+
+
+function person(name) {
+
+ console.log("You "+ name +" are handsome!!!")
+console.log("You "+name+" are smart!!!")
+console.log("You "+name+" are loyal!!!")
+console.log("You "+name+" are hardworker!!!")
+console.log("You "+name+" are smartworker!!!")
+
+    }
+person("vijay")
+
+person("Sanajy")
+
+
+function sum(a,b) {
+    return (a+b);
+    
+}
+let result=sum(12,12)
+let result2=sum(12,0)
+let result3=sum(12,20)
+
+console.log("Addition of 2 numbers are:"+result)
+console.log("Addition of 2 numbers are:"+result2)
+console.log("Addition of 2 numbers are:"+result3)
+
+
+
+const fun1 =(x) =>{
+    console.log("Values : "+x)
+
+}
+fun1(23)
+fun1(24)
+fun1(25)
+fun1(26)
