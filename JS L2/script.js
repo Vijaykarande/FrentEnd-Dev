05f94804e50ac5e0b7a4895d7b75fceb596cd47c
Vijaykarande <vijaykarande28@gmail.com>
2024-09-07 16:49:02 +0530

    JS vaiables primitives and object

---
+++ JS L2/script.js
@@ -0,0 +1,40 @@
+console.log("Hey I am Vijay Karande")
+
+// alert("Hello")
+
+var a=10;
+a=100;
+var b=100;
+
+var c="Vijay Karande"
+let a5=400;
+
+console.log(a+b+10)
+
+console.log(typeof a, typeof b, typeof c, typeof d)
+
+{
+    let a5=700;
+    console.log(a5, typeof a5)
+}
+
+let a11=null;
+let a22=12;
+let a33="Vijay Karande";
+let a44="Symbol";
+let a55=true;
+let a66=undefined;
+let a77=67.67;
+
+
+
+console.log(a11,a22,a33,a44,a55,a66,a77)
+console.log(typeof a11,typeof a22,typeof a33,typeof a44,typeof a55,typeof a66,typeof a77)
+
+
+let obj={
+    name:"Vijay Karande", PhoneNo:"751796202", Marks:"90%"
+}
+console.log(obj)
+obj.salary="700cr"
+console.log(obj)
