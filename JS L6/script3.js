3f199ca94d2482b56e6c06a88ea131e5c650ad19
VijayDemo <vijaykarande28@gmail.com>
2024-09-13 15:54:52 +0530

    Js Strings

---
+++ JS L6/script3.js
@@ -0,0 +1,40 @@
+
+console.log("This IS String Basics")
+
+let a="Vijay Karande"
+console.log(a)
+
+console.log(a[0])
+console.log(a.indexOf("V"))
+
+
+let f_name="Vijay"
+let l_name="karande"
+
+console.log("First Name is " + f_name + " last name is " + l_name)
+
+console.log(`first name ${f_name} and last name ${l_name}`)
+
+
+let d="Prajakta"
+let e="Vaibhav"
+console.log(d)
+console.log(d.length)
+console.log(d.indexOf("P"))
+console.log(d.charAt(7))
+
+console.log(d.toUpperCase())
+console.log(d.toLowerCase())
+
+console.log(d.slice(1,4))
+console.log(d.slice(0))
+
+console.log(d.replace("Pra", "Via"))
+console.log(d)
+
+let a1="Vijay"
+let a2="Karande"
+console.log(a1.concat("karande","Smart Boy"))
+
+
+
